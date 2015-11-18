fe1ccb1bc92cdac765c41e6a7c2973c07dd18aec
idriyslowe <idriyswins@gmail.com>
2015-11-17 20:21:02 -0600

    Everything

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,19 +18,22 @@ class ProductsController < ApplicationController
   def new
   end
   def create #@sock variable unneccesary since view doesn't call the variable.
-    @product = Product.create(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    product = Product.create(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
     redirect_to "/products"
   end
   def edit
     @product = Product.find_by(id: params[:id])
   end
   def update
-    @product = Product.find_by(id: params[:id])
-    @product.update(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
-    redirect_to "/products/#{@product.id}"
+    product = Product.find_by(id: params[:id])
+    product.update(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    flash[:success] = "Hey! You updated this product!"
+    redirect_to "/products/#{product.id}"
   end
   def destroy
     product = Product.find_by(id: params[:id])
     product.destroy
+    flash[:danger] = "Product DESTROYED!"
+    redirect_to '/products'
   end
 end
