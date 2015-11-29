f539137aeee5f66d444085b440edf1832e6ed947
idriyslowe <idriyswins@gmail.com>
2015-11-29 09:22:37 -0600

    added heroku

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,20 @@
 class OrdersController < ApplicationController
   def create
+    product = Product.find_by(id: params[:product_id])
+    price = product.price
+    quantity = params[:quantity].to_i
+    subtotal = price * quantity
+    tax = subtotal * Product::SALES_TAX
+    total = subtotal + tax
+
     order = Order.create(
       quantity: params[:quantity], 
       user_id: current_user.id,
-      product_id: params[:product_id])
+      product_id: params[:product_id],
+      subtotal: subtotal,
+      tax: tax, 
+      total: total
+      )
     redirect_to "/orders/#{order.id}"
   end
   def show
