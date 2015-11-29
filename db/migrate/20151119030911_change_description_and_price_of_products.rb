9dc9a7fd58b5b25d20edef1e8d78dcc96514c92b
idriyslowe <idriyswins@gmail.com>
2015-11-29 10:21:41 -0600

    casting price attribute to numeric

--- db/migrate/20151119030911_change_description_and_price_of_products.rb
+++ db/migrate/20151119030911_change_description_and_price_of_products.rb
@@ -1,6 +1,6 @@
 class ChangeDescriptionAndPriceOfProducts < ActiveRecord::Migration
   def change
     change_column :products, :description, :text
-    change_column :products, :price, :decimal, precision: 6, scale: 2
+    change_column :products, :price, 'numeric USING CAST(:price AS numeric)', precision: 6, scale: 2
   end
 end
