6cb0f93e522ee27057b36a16c1582b6457735570
idriyslowe <idriyswins@gmail.com>
2015-11-29 12:49:10 -0600

    removed extra methods from order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,16 +2,4 @@ class Order < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
 
-  def subtotal
-    product.price * quantity
-  end
-
-  def product_tax
-    product.tax * quantity
-  end
-
-  def order_total
-    subtotal + product_tax
-  end
-
 end
